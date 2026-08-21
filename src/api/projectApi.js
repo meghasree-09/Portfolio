4ce3c4fe0e5ad7344b83bd80f959b46375334d83
meghasree-09 <saimeghanasree09@gmail.com>
2026-08-21 23:23:36 +0530

    update project api

--- src/api/projectApi.js
+++ src/api/projectApi.js
@@ -2,12 +2,60 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8000/api/projects";
 
+const getAuthHeaders = () => {
+  const token = sessionStorage.getItem("adminToken");
+
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+};
+
+
 export const getProjects = async () => {
   const response = await axios.get(API_URL);
+
   return response.data;
 };
 
+
 export const getProject = async (id) => {
-  const response = await axios.get(`${API_URL}/${id}`);
+  const response = await axios.get(
+    `${API_URL}/${id}`
+  );
+
+  return response.data;
+};
+
+
+export const createProject = async (project) => {
+  const response = await axios.post(
+    API_URL,
+    project,
+    getAuthHeaders()
+  );
+
+  return response.data;
+};
+
+
+export const updateProject = async (id, project) => {
+  const response = await axios.put(
+    `${API_URL}/${id}`,
+    project,
+    getAuthHeaders()
+  );
+
+  return response.data;
+};
+
+
+export const deleteProject = async (id) => {
+  const response = await axios.delete(
+    `${API_URL}/${id}`,
+    getAuthHeaders()
+  );
+
   return response.data;
 };
